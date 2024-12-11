860ab6b1205615f0025aa745f21e84de82dd3d6a
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2024-12-11 19:53:25 +0100

    create first task list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import tasks from "./data/tasks";
+import HeaderComponent from "./components/HeaderComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const firstList = tasks.filter((task) => {
+    return task.state === "backlog" || task.state === "in_progress";
+  });
+
+  const firstListItem = firstList.map((task, index) => (
+    <li key={index}>{task.title}</li>
+  ))
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HeaderComponent />
+      <main>
+        <ul>{firstListItem}</ul>
+        <ul></ul>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/*
+Mostriamo in due liste diverse gli elementi dell’array tasks. Nella prima lista mostriamo solo i task (title, priority e estimatedTime) che hanno stato “backlog” o “in_progress”. Nella seconda lista mostriamo i task che hanno stato “completed”.
+Infine riprodurre il layout lasciato in allegato.
+*BONUS*
+Mostrare lo stato di ogni task in un piccolo tag di fianco al titolo, associando però ad ogni stato un colore diverso.
+*/
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,10 @@
+function HeaderComponent() {
+    const title = "JSX Task List";
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export default HeaderComponent;
